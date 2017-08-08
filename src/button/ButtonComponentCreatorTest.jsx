d566e737d043f85e9049243461161aa119766765
Daniel Mason <daniel@danielmason.com>
2017-08-08 09:29:13 +0100

    Added a test to make sure the onclick is passed throught the button HOC correctly

--- src/button/ButtonComponentCreatorTest.jsx
+++ src/button/ButtonComponentCreatorTest.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import createButton from './ButtonComponentCreator';
 import PaperIcon from '../icons/paper/PaperIconComponent';
 
+// These tests could be simpler. Breaking PaperIcon will break the wrong test, but it'll do for now
 describe('GameComponent', () => {
   it('should be a high order component', () => {
-    // This test could be simpler. Breaking PaperIcon will break the wrong test. But it'll do for now
     const PaperButton = createButton(PaperIcon);
-    const wrapper = shallow(<PaperButton />);
+    const wrapper = shallow(<PaperButton onclick={() => {}} />);
     expect(wrapper.contains(<PaperIcon />)).to.be.true;
+  });
+
+  it('resulting component should get it\'s onclick called', () => {
+    const spy = sinon.spy();
+    const PaperButton = createButton(PaperIcon);
+    const wrapper = shallow(<PaperButton onclick={spy} />);
+    wrapper.simulate('click');
+    expect(spy.calledOnce).to.be.true;
   });
 });
